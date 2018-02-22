ce8b054eb3b0b678a379bbc85e7798139bb2d7db
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2018-02-22 21:26:39 +0000

    Wrote the shooter shoot command.
    Implemented Prepare as the Shooter's default command, so it will run
    whenever possible.

---
+++ Robot/src/org/usfirst/frc/team4453/robot/commands/ShooterFire.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team4453.robot.commands;
+
+import org.usfirst.frc.team4453.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.command.TimedCommand;
+
+/**
+ *
+ */
+public class ShooterFire extends TimedCommand {
+
+    public ShooterFire() {
+	super(3); // TODO: How long to wait?
+        requires(Robot.shooter);
+    }
+
+    protected void initialize() {
+	Robot.shooter.fire();
+    }
+}
