f88757009f806eb69ba6ddab1ec10e1fec25d34b
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2018-02-28 17:27:27 -0500

    Made GrabberThrow a timed command.

--- Robot/src/org/usfirst/frc/team4453/robot/commands/GrabberThrow.java
+++ Robot/src/org/usfirst/frc/team4453/robot/commands/GrabberThrow.java
@@ -2,21 +2,19 @@ package org.usfirst.frc.team4453.robot.commands;
 
 import org.usfirst.frc.team4453.robot.Robot;
 
-import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 
 /**
  *
  */
-public class GrabberThrow extends InstantCommand {
+public class GrabberThrow extends TimedCommand {
 
     public GrabberThrow() {
-	super();
+	super(.5);
 	requires(Robot.grabber);
     }
-
-    // Called once when the command executes
-    @Override
-    protected void initialize() {
+    
+    protected void execute() {
 	Robot.grabber.toss();
     }
 
